860f8357aed01a69ded3558dfbaa5ced9a64db11
kellyeryan <kellyeryan@gmail.com>
2019-07-15 17:40:15 -0400

    Finish oxford comma lab

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
 
+def oxford_comma(array)
+  if array.count ==1
+    array.join
+  elsif
+    array.count == 2
+    array.join( " and ")
+  else
+    array[0...-1].join(", ") + ", and " + array.last
+  end
 end
+
